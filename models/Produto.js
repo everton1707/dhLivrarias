22eddf113f8dc473817712c1709916318ecdb325
everton1707 <everton.leema@gmail.com>
2022-05-26 22:04:25 -0300

    atualização configuração sequelize

--- models/Produto.js
+++ models/Produto.js
@@ -1,14 +1,18 @@
 const { DataTypes } = require("sequelize/types");
 const { sequelize } = require(".");
 
-module.exports = (sequelize, DataTypes)=>{
-    const Usuario = sequelize.define('Usuario',{
+const ProdutoModel = (sequelize, DataTypes)=>{
+    const Produto = sequelize.define('Produto',{
         nome: DataTypes.STRING,
         descricao: DataTypes.STRING,
         preco: DataTypes.float,
-        /*campos do banco de dados*/ 
+        /*campos do banco de dados*/
     },{
         tableName: 'produtos',
         timestamp: false
     })
+
+    return Produto;
 }
+
+module.exports = ProdutoModel;
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,15 @@ const produtoController = require('../controllers/produtoController');
 router.get('/', produtoController.index);
 
 router.get('/faleConosco', produtoController.faleConosco);
+router.get('/usuario', produtoController.usuario);
 
+router.get('/login',produtoController.login);
+router.get('/cadastro',produtoController.cadastro);
+router.get('/finalizacao', produtoController.finalizacao);
+router.get('/checkout', produtoController.checkout);
+router.get('/carrinho', produtoController.carrinho);
+router.get('/produto', produtoController.produto);
+router.get('/produto', produtoController.produto);
 
 
 
