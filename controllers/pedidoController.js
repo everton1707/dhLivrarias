e1c6e15592c0d41ed35b109d9982674639be480e
everton1707 <everton.leema@gmail.com>
2022-08-01 12:55:27 -0300

    atualizando branch

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -15,9 +15,8 @@ const pedidoController = {
 
         })
 
-        // consultar itens do carrinho e adicionalos a variavel
+
         if (pedido == null) {
-            // criando novo pedido
             pedido = await db.Pedido.create({
                 data: new Date(),
                 valor: 0,
@@ -70,10 +69,7 @@ const pedidoController = {
             }, {
                 include: ["produtos"]
             })
-            /*      TESTE
-                        console.log("**********************  Pedido Criado  **********************")
-                        console.log(pedidoCriado.id);
-                        pedido = pedidoCriado;*/
+            
         }
         const resultado = await db.Pedido_has_produto.findOne({
             where: {
@@ -81,8 +77,8 @@ const pedidoController = {
                 produto_id: produto.id,
             }
         })
-        // fazer verificação se existe esse produto no carrinho 
-        // criar registro na tabela de pedido_has_produtos 
+ 
+
         if (resultado == null) {
 
             await db.Pedido_has_produto.create({
@@ -117,7 +113,7 @@ const pedidoController = {
         res.redirect('/carrinho');
     },
     pagamento: async (req, res) => {
-        //verificar se o id do pedido pertence ao usuario
+
         const pedido = await db.Pedido.findOne({
             where: {
                 id: req.params.id
@@ -153,12 +149,7 @@ const pedidoController = {
     cancelar: (req, res) => {
 
     },
-    consultar: (req, res) => {//se existe
 
-    },
-    criar: (req, res) => {
-
-    },
 }
 
 module.exports = pedidoController;
